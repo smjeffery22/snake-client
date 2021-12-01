45ae0e8db8acaa7c752d65329cdb14a6002875c0
Jeffery <parkjon8@gmail.com>
2021-12-01 23:30:48 +0000

    Included messages

--- input.js
+++ input.js
@@ -21,20 +21,32 @@ const handleUserInput = function(key) {
     process.exit();
   }
 
-  if (key === 'w') {
-    connection.write("Move: up")
+  if (key === "w") {
+    connection.write("Move: up");
   }
 
-  if (key === 'a') {
-    connection.write("Move: left")
+  if (key === "a") {
+    connection.write("Move: left");
   }
 
-  if (key === 's') {
-    connection.write("Move: down")
+  if (key === "s") {
+    connection.write("Move: down");
   }
 
-  if (key === 'd') {
-    connection.write("Move: right")
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+
+  if (key === "1") {
+    connection.write("Say: Hello!");
+  }
+
+  if (key === "2") {
+    connection.write("Say: Move!");
+
+  if (key === "3") {
+    connection.write("Say: Boo!");
+  }
   }
 };
 
