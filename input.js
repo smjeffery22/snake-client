b4ee9159d059fda2292fbb662b9197c6063e41ac
Jeffery <parkjon8@gmail.com>
2021-12-01 23:21:45 +0000

    Added movement commands in input.js and implemented in play.js

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
-// setup interface to handle user input from stdin
-const setupInput = function() {
+const readline = require('readline');
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,9 +12,29 @@ const setupInput = function() {
   return stdin;
 }
 
-const handleUserInput = function() {
-  if ('\u0003') {
+// // setup interface to handle user input from stdin
+// const setupInput = function() {
+// }
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
     process.exit();
+  }
+
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+
+  if (key === 's') {
+    connection.write("Move: down")
+  }
+
+  if (key === 'd') {
+    connection.write("Move: right")
   }
 };
 
